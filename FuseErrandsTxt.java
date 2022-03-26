d8672507148f517bb740c0c644ee026e39bbb7ec
Sridhar Sarnobat.deprecated <sarnobat@Sridhars-MacBook-Air.local>
2022-03-25 22:33:22 -0700

    Minor changes (untested)

--- FuseErrandsTxt.java
+++ FuseErrandsTxt.java
@@ -1,8 +1,5 @@
-import java.io.BufferedOutputStream;
-import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.nio.ByteBuffer;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.FileAlreadyExistsException;
@@ -24,9 +21,6 @@ import net.fusejna.util.FuseFilesystemAdapterFull;
 
 // 2022-03: going forward, I'm using Java 11. No neeed for groovy anymore
 // https://github.com/EtiennePerot/fuse-jna/blob/master/src/main/java/net/fusejna/util/FuseFilesystemAdapterFull.java
-//
-// Copied from:
-// 		https://github.com/EtiennePerot/fuse-jna/blob/master/src/main/java/net/fusejna/examples/MemoryFS.java
 public class FuseErrandsTxt {
 
 	public static void main(String[] args) throws FuseException, IOException {
@@ -90,7 +84,7 @@ public class FuseErrandsTxt {
 			final String fileContents = contents2;
 			System.out.println("FuseErrandsTxt.HelloFS1.read2() B fileContents = " + fileContents);
 			try {
-				buffer.put(fileContents.getBytes());
+			buffer.put(fileContents.getBytes());
 			} catch (Exception e) {
 				e.printStackTrace();
 			}
@@ -144,18 +138,6 @@ public class FuseErrandsTxt {
 				contents2 = new String(bytesToWrite, StandardCharsets.UTF_8);
 				contents.position(0); // Rewind
 			}
-//			try {
-//				Process process = new ProcessBuilder().command("cat", path).start();
-//				BufferedReader stdOut = new BufferedReader(new InputStreamReader(process.getInputStream()));
-//				String line;
-//				while ((line = stdOut.readLine()) != null) {
-//					System.out.println("FuseErrandsTxt.HelloFS1.write() SRIDHAR " + line);
-//				}
-//
-//			} catch (IOException e) {
-//				e.printStackTrace();
-//			}
-
 			return (int) bufSize;
 		}
 	}
